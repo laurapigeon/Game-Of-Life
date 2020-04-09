305591517bcc7e7beba1671c67438ff81c815b9c
Laura <laurahannah44@gmail.com>
2020-04-09 14:02:31 +0100

    Comments and bugfixes

--- game_of_life.py
+++ game_of_life.py
@@ -5,58 +5,66 @@ pygame.init()
 
 
 class Board:
-    def __init__(self, screen_dims):
-        self.tile_dims = TILE_DIMS
+    def __init__(self, tile_dims, screen_dims):
+        self.tile_dims = tile_dims
         self.update_dimensions(screen_dims)
 
     def update_dimensions(self, screen_dims):
+        # new dimensions saved ready for next population
         self.new_columns = screen_dims[0] // self.tile_dims[0]
         self.new_rows = screen_dims[1] // self.tile_dims[1]
 
     def get_size(self, old=False):
-        if old:
+        if old:  # whether to pass the current set of dimensions or the new set (screen rect)
             return (self.columns * self.tile_dims[0], self.rows * self.tile_dims[1])
         else:
             return (self.new_columns * self.tile_dims[0], self.new_rows * self.tile_dims[1])
 
     def flip_tile(self, coords):
+        # for when tile is clicked
         board_size = self.get_size(old=True)
         if coords[0] < board_size[0] and coords[1] < board_size[1]:
             coords[0] //= self.tile_dims[0]
             coords[1] //= self.tile_dims[1]
             self.tiles[coords[1]][coords[0]].change_type(flip=True)
 
-    def populate(self, percentage=0.5, seed=None):
+    def populate(self, percentage, seed=None):
+        # update values
         self.tiles = []
         self.living_cells = 0
         self.generation = 0
         self.columns = self.new_columns
         self.rows = self.new_rows
 
-        dimensions_string = str(self.columns) + "x" + str(self.rows)
-        if type(percentage) in (int, float):
-            percentage_string = str(percentage * 100) + "% density"
-        else:
-            percentage_string = percentage
-        mode_code = "b" + "".join([str(num) for num in RULES[mode][0]]) + "s" + "".join([str(num) for num in RULES[mode][1]])
-        mode_string = "{}-Type Mode: {} ({})".format(RULES[mode][3], RULES[mode][2], mode_code)
         if OUTPUT_TO_FILE is not False:
+            # empty and create title line for population.txt
+            dimensions_string = str(self.columns) + "x" + str(self.rows)
+            if type(percentage) in (int, float):
+                percentage_string = str(percentage * 100) + "% density"
+            else:
+                percentage_string = percentage
+            mode_code = "b" + "".join([str(num) for num in RULES[mode][0]]) + "s" + "".join([str(num) for num in RULES[mode][1]])
+            mode_string = "{}-Type Mode: {} ({})".format(RULES[mode][3], RULES[mode][2], mode_code)
             open("population.txt", "w").write(dimensions_string + " " + percentage_string + " " + mode_string)
 
         colour_combo = list(range(5))
         random.shuffle(colour_combo)
 
         random.seed(seed)
+
         for y in range(self.rows):
             row = []
             for x in range(self.columns):
+                # possible colour segments dependant on position
                 colours = (round(x * 255 / self.columns),
                            round(y * 255 / self.rows),
                            255 - round(x * 255 / self.columns),
                            255 - round(y * 255 / self.rows),
                            255)
+                # random 3 of the 5 possibilities
                 colour = (colours[colour_combo[0]], colours[colour_combo[1]], colours[colour_combo[2]])
 
+                # determine whether tile is living or dead
                 if percentage == "checkerboard":
                     tile_state = (x + y) % 2 == 1
                 elif percentage == "lines":
@@ -66,17 +74,10 @@ class Board:
                 else:
                     tile_state = random.random() < percentage
 
-                self.living_cells += (-1, 1)[tile_state]
+                self.living_cells += tile_state
                 row.append(Tile(self, tile_state, (x, y), colour))
 
             self.tiles.append(row)
-
-        if OUTPUT_TO_FILE is not False:
-            if OUTPUT_TO_FILE == "cells":
-                open("population.txt", "a").write("\n" + str(self.living_cells))
-            elif OUTPUT_TO_FILE == "percentage":
-                size = self.get_size(old=True)
-                open("population.txt", "a").write("\n" + str(round(100 * self.living_cells / self.columns / self.rows, 2)))
 
     def neighbor_check(self):
         for row in self.tiles:
@@ -84,6 +85,7 @@ class Board:
                 x = tile.column
                 y = tile.row
                 neighbors = list()
+                # creates neighbors based on position of tile
                 if x != 0:
                     neighbors.append(self.tiles[y][x - 1])
                 if x != self.columns - 1:
@@ -107,27 +109,31 @@ class Board:
                 tile.check_neighbors(neighbors)
 
     def update_tiles(self):
+        # append value to tile before changing tiles
+        if OUTPUT_TO_FILE == "value":
+            open("population.txt", "a").write("\n" + str(self.living_cells))
+        elif OUTPUT_TO_FILE == "percentage":
+            open("population.txt", "a").write("\n" + str(round(100 * self.living_cells / self.columns / self.rows, 2)))
+
+        # tiles switched to their new state
         for row in self.tiles:
             for tile in row:
                 tile.change_type()
-        if OUTPUT_TO_FILE is not False:
-            if OUTPUT_TO_FILE == "value":
-                open("population.txt", "a").write("\n" + str(self.living_cells))
-            elif OUTPUT_TO_FILE == "percentage":
-                open("population.txt", "a").write("\n" + str(round(100 * self.living_cells / self.columns / self.rows, 2)))
         self.generation += 1
 
-    def draw_tiles(self):
+    def draw_tiles(self, surface):
+        # draw each board tile on the surface
         for row in self.tiles:
             for tile in row:
-                if tile.state:
+                if tile.state:  # if living
                     rect = tile.get_rect()
-                    pygame.draw.rect(screen, tile.get_colour(), rect)
+                    pygame.draw.rect(surface, tile.get_colour(), rect)
 
-    def draw(self):
+    def draw(self, surface):
+        # bounding box for board when surface size is different
         size = self.get_size(old=True)
         rect = pygame.Rect(-1, -1, size[0] + 2, size[1] + 2)
-        pygame.draw.rect(screen, (255, 255, 255), rect, 1)
+        pygame.draw.rect(surface, (255, 255, 255), rect, 1)
 
 
 class Tile:
@@ -146,6 +152,7 @@ class Tile:
             self.state = self.new_state
 
     def check_neighbors(self, neighbors):
+        # change state based on ruleset and living neighbors
         alive_neighbors = 0
         for neighbor in neighbors:
             if neighbor.state == 1:
@@ -178,20 +185,10 @@ class Tile:
 
 class ScreenPrint:
     @staticmethod
-    def get_surface(string, frame=None, print_string=True):
+    def get_surface(string, location="bottomright", frame=None, print_string=True):
+        # returns a dict of the text surface and rect and birth frame
         if DISPLAY_TEXT:
-            return {"surface": FONT.render(string, True, (255, 255, 255)),
-                    "size": FONT.size(string),
-                    "birthframe": frame}
-        else:
-            if print_string:
-                print(string)
-            return None
-
-    @staticmethod
-    def display_surface(surface, board, location, frame=None):
-        if surface is not None:
-            rect = pygame.Rect(0, 0, *surface["size"])
+            rect = pygame.Rect(0, 0, *FONT.size(string))
             if location == "bottomright":
                 rect.bottomright = board.get_size()
             elif location == "bottomleft":
@@ -200,8 +197,19 @@ class ScreenPrint:
                 rect.topright = (board.get_size()[0], 0)
             elif location == "topleft":
                 rect.topleft = (0, 0)
+            return {"surface": FONT.render(string, True, (255, 255, 255)),
+                    "rect": rect,
+                    "birthframe": frame}
+        else:
+            if print_string:
+                print(string)
+            return None
 
-            screen.blit(surface["surface"], rect)
+    @staticmethod
+    def display_surface(surface, board, frame=None):
+        # takes in and ages the curent priority surface
+        if surface is not None:
+            screen.blit(surface["surface"], surface["rect"])
 
             if surface["birthframe"] is not None and frame is not None:
                 if frame - surface["birthframe"] >= 60:
@@ -215,13 +223,16 @@ class ScreenPrint:
 #region definitions
 TILE_DIMS = (10, 10)
 SCREEN_DIMS = (1080, 720)
+board = Board(TILE_DIMS, SCREEN_DIMS)
 FIXED_DIMS = False
-COLOUR = True
+
 DISPLAY_TEXT = True
 OUTPUT_TO_FILE = "value"  # None, "value" or "percentage"
-SEED = 44
+
 MAX_FRAMERATE = 30
 DEFAULT_SLOW_AMOUNT = 1
+slow_amount = DEFAULT_SLOW_AMOUNT
+
 RULES = [
         [[1, 3, 5, 7], [1, 3, 5, 7],                "Replicator",         "Replication"],
         [[1, 3, 5, 7], [0, 2, 4, 6, 8],             "Fredkin",            "Replication"],
@@ -237,15 +248,16 @@ RULES = [
         [[3, 8],       [2, 3],                      "Pedestrian Life",    "Life"],
         [[3],          [1, 2],                      "Flock",              "Life"],
         [[3, 6],       [1, 2, 5],                   "2x2",                "Life"],
-        [[3, 6, 7, 8], [3, 4, 6, 7, 8],             "Day & Night",        "Life"],
+        [[3, 6, 7, 8], [3, 4, 6, 7, 8],             "Day & Night",        "Life"]
 ]
 DEFAULT_MODE = 7
-FONT = pygame.font.SysFont("Calibri", round(board.get_size()[1] / 20))
-
-board = Board(SCREEN_DIMS)
-board.populate(0, seed=SEED)
-slow_amount = DEFAULT_SLOW_AMOUNT
 mode = DEFAULT_MODE
+
+COLOUR = True
+SEED = 44
+board.populate(0, seed=SEED)
+
+FONT = pygame.font.SysFont("Calibri", round(board.get_size()[1] / 20))
 text_surface = None
 
 pygame.display.set_caption("The Game of Life")
@@ -263,30 +275,34 @@ while not done:
         if event.type == pygame.QUIT:
             done = True
         if event.type == pygame.VIDEORESIZE:
-            if FIXED_DIMS:
+            if FIXED_DIMS:  # allows screen to any size regardless of board
                 screen = pygame.display.set_mode((event.w, event.h), pygame.RESIZABLE)
-            else:
+            else:  # locks screen size to nearest whole tile size and updates board
                 board.update_dimensions((event.w, event.h))
                 FONT = pygame.font.SysFont("Calibri", round(board.get_size()[1] / 20))
                 screen = pygame.display.set_mode(board.get_size(), pygame.RESIZABLE)
 
         if event.type == pygame.KEYDOWN:
+            # pause
             if event.key == pygame.K_SPACE:
                 pause = not pause
-                text_surface = ScreenPrint.get_surface(("Unpaused", "Paused")[pause], frame)
+                text_surface = ScreenPrint.get_surface(("Unpaused", "Paused")[pause], "bottomright", frame)
 
+            # frame advance
             if event.key == pygame.K_RETURN and pause:
                 board.neighbor_check()
                 board.update_tiles()
-                text_surface = ScreenPrint.get_surface("Frame advanced", frame)
+                text_surface = ScreenPrint.get_surface("Frame advanced", "bottomright", frame)
 
+            # change fraction of max framerate
             if event.key in (pygame.K_PERIOD, pygame.K_COMMA):
                 if event.unicode == "." and slow_amount > 1.5:
                     slow_amount -= 1
                 elif event.unicode == "," and slow_amount < MAX_FRAMERATE - 0.5:
                     slow_amount += 1
-                text_surface = ScreenPrint.get_surface("{} frames per second".format(round(MAX_FRAMERATE / slow_amount, 1)), frame)
+                text_surface = ScreenPrint.get_surface("{} frames per second".format(round(MAX_FRAMERATE / slow_amount, 1)), "bottomright", frame)
 
+            # change mode
             if event.key in (pygame.K_EQUALS, pygame.K_MINUS):
                 if event.unicode == "=":
                     mode += 1
@@ -297,8 +313,9 @@ while not done:
                 mode_string = "{}-Type Mode: {} ({})".format(RULES[mode][3], RULES[mode][2], mode_code)
                 if OUTPUT_TO_FILE is not False:
                     open("population.txt", "a").write("\nMode changed to " + mode_string)
-                text_surface = ScreenPrint.get_surface(mode_string, frame)
+                text_surface = ScreenPrint.get_surface(mode_string, "bottomright", frame)
 
+            # populate board
             key = event.unicode
             if key == "c":
                 board.populate("checkerboard")
@@ -310,20 +327,22 @@ while not done:
                 board.populate(int(key) / 10, seed=SEED)
 
         if event.type == pygame.MOUSEBUTTONDOWN:
+            # flip cell on click
             if event.button == 1:
                 coords = list(pygame.mouse.get_pos())
                 board.flip_tile(coords)
     #endregion
 
+    # reduce calculations to every n frames
     if frame % slow_amount == 0:
         screen.fill((0, 0, 0))
-        board.draw_tiles()
-        board.draw()
+        board.draw_tiles(screen)
+        board.draw(screen)
 
         if DISPLAY_TEXT:
-            text_surface = ScreenPrint.display_surface(text_surface, board, "bottomright", frame=frame)
+            text_surface = ScreenPrint.display_surface(text_surface, board, frame)
 
-        ScreenPrint.display_surface(ScreenPrint.get_surface(str(board.generation), print_string=False), board, "bottomleft")
+        ScreenPrint.display_surface(ScreenPrint.get_surface(str(board.generation), "bottomleft", print_string=False), board)
 
         pygame.display.flip()
 
@@ -333,5 +352,6 @@ while not done:
 
     frame += 1
 
+    # cap framerate to avoid lagging
     if MAX_FRAMERATE is not None:
         clock.tick(MAX_FRAMERATE)
